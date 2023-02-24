98949d691e4ac9eb370365210e8455919b3c3ac3
Peter Kejlberg Madsen <peter@mangrove.exchange>
2023-02-24 09:03:49 +0100

    added exchange options

--- src/MgvArbitrage.sol
+++ src/MgvArbitrage.sol
@@ -6,15 +6,22 @@ import {IMangrove} from "mgv_src/IMangrove.sol";
 import {IERC20} from "mgv_src/MgvLib.sol";
 import {AccessControlled} from "mgv_src/strategies/utils/AccessControlled.sol";
 
-struct ArbParams {
-    address taker;
-    uint256 offerId;
-    address wantsToken;
-    uint256 wants;
-    address givesToken;
-    uint256 gives;
-    uint24 fee;
+enum Exchange {
+    MANGROVE,
+    UNISWAP,
+    NONE
 }
+
+struct ArbParams {
+    uint256 offerId;
+    address takerWantsToken;
+    uint256 takerWants;
+    address takerGivesToken;
+    uint256 takerGives;
+    uint24 fee;
+    uint256 minGain;
+}
+
 
 contract MgvArbitrage is AccessControlled {
     IMangrove mgv;
@@ -27,19 +34,51 @@ contract MgvArbitrage is AccessControlled {
 
     receive() external payable virtual {}
 
-    function doArbitrage(ArbParams calldata params) external onlyAdmin returns (uint256 amountOut) {
-        uint256[4][] memory targets = new uint[4][](1);
-        targets[0] = [params.offerId, params.wants, params.gives, type(uint256).max];
-        (uint256 successes, uint256 takerGot, uint256 takerGave,,) =
-            mgv.snipesFor(params.wantsToken, params.givesToken, targets, false, params.taker);
-        require(successes == 1, "MgvArbitrage/snipeFail");
+    uint256 public MAX_GASREQ = 350000;
 
-        IERC20(params.wantsToken).transferFrom(params.taker, address(this), takerGot);
+    function withdrawToken(address token, uint256 amount, address to) external onlyAdmin returns (bool) {
+        return IERC20(token).transfer(to, amount);
+    }
+
+    function doArbitrageExchangeOnUniswap(ArbParams calldata params, address token, uint24 fee) external onlyAdmin returns (uint256 amountOut) {
+        uint256 givesBalance = IERC20(params.takerGivesToken).balanceOf(address(this)); // Important that this is done before any tranfers
+        uint256 amountIn = preExchangeOnUniswap(params, token, fee);
+        (uint256 takerGot, uint256 takerGave) = snipeOnMgv(params);
+        amountOut = swapOnUniswap(params, takerGot, takerGave);
+        postExchangeOnUniswap(params, token, fee, amountIn);
+        checkGain( params.takerGivesToken, givesBalance, params.minGain);
+    }
+
+    function doArbitrageExchangeOnMgv(ArbParams calldata params, address token) external onlyAdmin returns (uint256 amountOut) {
+        uint256 givesBalance = IERC20(params.takerGivesToken).balanceOf(address(this)); // Important that this is done before any tranfers
+        uint256 amountIn = preExchangeOnMgv(params, token);
+        (uint256 takerGot, uint256 takerGave) = snipeOnMgv(params);
+        amountOut = swapOnUniswap(params, takerGot, takerGave);
+        postExchangeOnMgv(params, token, amountIn);
+        checkGain( params.takerGivesToken, givesBalance, params.minGain);
+    }
+
+    function doArbitrage(ArbParams calldata params) external onlyAdmin returns (uint256 amountOut) {
+        uint256 givesBalance = IERC20(params.takerGivesToken).balanceOf(address(this)); // Important that this is done before any tranfers
+        (uint256 takerGot, uint256 takerGave) = snipeOnMgv(params);
+        amountOut = swapOnUniswap(params, takerGot, takerGave);
+        checkGain( params.takerGivesToken, givesBalance, params.minGain);
+    }
+
+    function checkGain(address takerGivesToken, uint256 givesBalance, uint256 minGain) view internal {
+        require(IERC20(takerGivesToken).balanceOf(address(this)) > givesBalance, "MgvArbitrage/notProfitable");
+        require(IERC20(takerGivesToken).balanceOf(address(this)) - givesBalance > minGain, "MgvArbitrage/notMinGain");
+    }
+
+    function swapOnUniswap(ArbParams calldata params, uint256 takerGot, uint256 takerGave)
+        internal
+        returns (uint256 amountOut)
+    {
         ISwapRouter.ExactInputSingleParams memory uniswapParams = ISwapRouter.ExactInputSingleParams({
-            tokenIn: params.wantsToken,
-            tokenOut: params.givesToken,
+            tokenIn: params.takerWantsToken,
+            tokenOut: params.takerGivesToken,
             fee: params.fee,
-            recipient: params.taker,
+            recipient: address(this),
             deadline: type(uint256).max,
             amountIn: takerGot,
             amountOutMinimum: takerGave,
@@ -49,8 +88,83 @@ contract MgvArbitrage is AccessControlled {
         require(amountOut >= takerGave, "MgvArbitrage/notProfitable");
     }
 
+    function snipeOnMgv(ArbParams calldata params) internal returns (uint256, uint256) {
+        uint256[4][] memory targets = new uint[4][](1);
+        targets[0] = [params.offerId, params.takerWants, params.takerGives, type(uint256).max];
+        (uint256 successes, uint256 takerGot, uint256 takerGave,,) =
+            mgv.snipes(params.takerWantsToken, params.takerGivesToken, targets, false);
+        require(successes == 1, "MgvArbitrage/snipeFail");
+        return (takerGot, takerGave);
+    }
+
+    function postExchangeOnUniswap(ArbParams calldata params, address token, uint24 fee, uint256 amountIn) internal {
+        if (token != address(0) && token != params.takerGivesToken) {
+            ISwapRouter.ExactOutputSingleParams memory exhcangeParams = ISwapRouter.ExactOutputSingleParams({
+                tokenIn: params.takerGivesToken,
+                tokenOut: token,
+                fee: fee,
+                recipient: address(this),
+                deadline: type(uint256).max,
+                amountOut: amountIn,
+                amountInMaximum: type(uint256).max,
+                sqrtPriceLimitX96: 0
+            });
+            router.exactOutputSingle(exhcangeParams);
+        }
+    }
+
+    function postExchangeOnMgv(ArbParams calldata params, address token, uint256 amountIn)
+        internal
+        returns (uint256)
+    {
+        if (token != address(0) && token != params.takerGivesToken) {
+            (uint256 takerGot,,,) = mgv.marketOrder({
+                outbound_tkn: token,
+                inbound_tkn: params.takerGivesToken,
+                takerWants: amountIn,
+                takerGives: type(uint160).max,
+                fillWants: true
+            });
+            require(takerGot == amountIn, "MgvArbitrage/notEnoughOnMgv");
+        }
+        return 0;
+    }
+
+    function preExchangeOnUniswap(ArbParams calldata params, address token, uint24 fee) internal returns (uint256) {
+        if (token != address(0) && token != params.takerGivesToken) {
+            ISwapRouter.ExactOutputSingleParams memory exhcangeParams = ISwapRouter.ExactOutputSingleParams({
+                tokenIn: token,
+                tokenOut: params.takerGivesToken,
+                fee: fee,
+                recipient: address(this),
+                deadline: type(uint256).max,
+                amountOut: params.takerGives,
+                amountInMaximum: IERC20(token).balanceOf(address(this)),
+                sqrtPriceLimitX96: 0
+            });
+            return router.exactOutputSingle(exhcangeParams);
+        }
+        return 0;
+    }
+
+    function preExchangeOnMgv(ArbParams calldata params, address token) internal returns (uint256) {
+        if (token != address(0) && token != params.takerGivesToken) {
+            (uint256 takerGot, uint256 takerGave,,) = mgv.marketOrder({
+                outbound_tkn: params.takerGivesToken,
+                inbound_tkn: token,
+                takerWants: params.takerGives,
+                takerGives: IERC20(token).balanceOf(address(this)),
+                fillWants: true
+            });
+            require(takerGot == params.takerGives, "MgvArbitrage/notEnoughOnMgv");
+            return takerGave;
+        }
+        return 0;
+    }
+
     function activateTokens(IERC20[] calldata tokens) external onlyAdmin {
         for (uint256 i = 0; i < tokens.length; ++i) {
+            tokens[i].approve(address(mgv), type(uint256).max);
             tokens[i].approve(address(router), type(uint256).max);
         }
     }
--- test/MgvArbitrage.t.sol
+++ test/MgvArbitrage.t.sol
@@ -3,158 +3,318 @@ pragma solidity ^0.8.10;
 
 import {MangroveTest} from "mgv_test/lib/MangroveTest.sol";
 
-import {PolygonFork, PinnedPolygonFork} from "mgv_test/lib/forks/Polygon.sol";
+import {PinnedPolygonFork} from "src/MyPinnedPolygon.sol"; // have to use ar polygon fork on a newer block
+// import {PinnedPolygonFork} from "mgv_test/lib/forks/Polygon.sol";
 import {TestToken} from "mgv_test/lib/tokens/TestToken.sol";
 import {IERC20} from "mgv_src/MgvLib.sol";
 import "src/MgvArbitrage.sol";
 
-
 contract MgvArbitrageTest is MangroveTest {
+    PinnedPolygonFork fork;
+    MgvArbitrage arbStrat;
+    IERC20 WETH;
+    IERC20 USDC;
+    IERC20 DAI;
 
-  PolygonFork fork;
-  MgvArbitrage arbStrat;
-  IERC20 WETH;
-  IERC20 USDC;
+    address payable taker;
+    address payable seller;
+    address payable lp;
 
-  address payable taker;
-  address payable seller;
+    receive() external payable virtual {}
 
-  receive() external payable virtual {}
-
-  function setUp() public override {
-    // use the pinned Polygon fork
-    fork = new PinnedPolygonFork(); // use polygon fork to use dai, usdc and weth addresses
-    fork.setUp();
-    mgv = setupMangrove();
-    WETH = IERC20(fork.get("WETH"));
-    USDC = IERC20(fork.get("USDC"));
-    setupMarket(WETH, USDC); 
-
-    taker = freshAddress();
-    fork.set("taker", taker);
-    seller = freshAddress();
-    fork.set("seller", seller);
-
-    deal(taker, 10 ether);
-    deal(seller, 10 ether);
+    function setUp() public override {
+        // use the pinned Polygon fork
+        fork = new PinnedPolygonFork(); // use polygon fork to use dai, usdc and weth addresses
+        fork.setUp();
+        mgv = setupMangrove();
+        WETH = IERC20(fork.get("WETH"));
+        USDC = IERC20(fork.get("USDC"));
+        DAI = IERC20(fork.get("DAI"));
+        setupMarket(WETH, USDC);
+        setupMarket(DAI, USDC);
 
 
-    vm.startPrank(taker);
-    USDC.approve($(mgv), type(uint).max);
-    WETH.approve($(mgv), type(uint).max);
-    vm.stopPrank();
+        taker = freshAddress();
+        fork.set("taker", taker);
+        seller = freshAddress();
+        fork.set("seller", seller);
+        lp = freshAddress();
+        fork.set("lp", lp);
 
-    vm.startPrank(seller);
-    WETH.approve(address(mgv), type(uint).max); 
-    vm.stopPrank();
+        deal($(USDC), lp, cash(USDC, 100000));
+        deal($(DAI), lp, cash(DAI, 100000));
+        deal(taker, 10 ether);
+        deal(seller, 10 ether);
+        deal(lp, 10 ether);
 
-    deployStrat();
-  }
+        vm.startPrank(taker);
+        USDC.approve($(mgv), type(uint256).max);
+        WETH.approve($(mgv), type(uint256).max);
+        vm.stopPrank();
 
-  function deployStrat() public {
-    arbStrat = new MgvArbitrage({
+        vm.startPrank(lp);
+        mgv.newOffer{value: 1 ether}({
+            outbound_tkn: $(USDC),
+            inbound_tkn: $(DAI),
+            wants: cash(DAI, 10000),
+            gives: cash(USDC, 10000),
+            gasreq: 50_000,
+            gasprice: 0,
+            pivotId: 0
+        });
+        mgv.newOffer({
+            outbound_tkn: $(DAI),
+            inbound_tkn: $(USDC),
+            wants: cash(USDC, 10000),
+            gives: cash(DAI, 10000),
+            gasreq: 50_000,
+            gasprice: 0,
+            pivotId: 0
+        });
+        USDC.approve($(mgv), type(uint).max);
+        DAI.approve($(mgv), type(uint).max);
+        vm.stopPrank();
+
+        vm.prank(seller);
+        WETH.approve(address(mgv), type(uint256).max);
+
+        deployStrat();
+    }
+
+    function deployStrat() public {
+        arbStrat = new MgvArbitrage({
       _mgv: IMangrove($(mgv)),
       admin: address(this)
       });
-    fork.set("MgvArbitrage", address(arbStrat));
+        fork.set("MgvArbitrage", address(arbStrat));
 
-    vm.startPrank(taker);
-    mgv.approve($(WETH), $(USDC), address(arbStrat), type(uint).max); 
-    mgv.approve($(USDC), $(WETH), address(arbStrat), type(uint).max); 
-    WETH.approve(address(arbStrat), type(uint).max);
-    vm.stopPrank();
-    IERC20[] memory tokens = new IERC20[](2);
-    tokens[0] = WETH;
-    tokens[1] = USDC;
+        vm.startPrank(taker);
+        WETH.approve(address(arbStrat), type(uint256).max);
+        vm.stopPrank();
+        IERC20[] memory tokens = new IERC20[](3);
+        tokens[0] = WETH;
+        tokens[1] = USDC;
+        tokens[2] = DAI;
 
-    arbStrat.activateTokens(tokens);
-  }
+        arbStrat.activateTokens(tokens);
+    }
 
-  function test_isProfitable() public {
-    deal( $(USDC), taker, cash(USDC, 20000)); 
-    deal( $(WETH), seller,cash(WETH, 10));
-    vm.prank(seller);
-    uint offerId = mgv.newOffer{value: 1 ether}({
-    outbound_tkn: $(WETH),
-    inbound_tkn: $(USDC),
-    wants: cash(USDC, 1000),
-    gives: cash(WETH, 1),
-    gasreq: 50_000 ,
-    gasprice: 0,
-    pivotId: 0
-    });
+    function test_isProfitable() public {
+        deal($(USDC), address(arbStrat), cash(USDC, 20000));
+        deal($(WETH), seller, cash(WETH, 10));
+        vm.prank(seller);
+        uint256 offerId = mgv.newOffer{value: 1 ether}({
+            outbound_tkn: $(WETH),
+            inbound_tkn: $(USDC),
+            wants: cash(USDC, 1000),
+            gives: cash(WETH, 1),
+            gasreq: 50_000,
+            gasprice: 0,
+            pivotId: 0
+        });
 
-    ArbParams memory params = ArbParams( { 
-        taker:taker, 
-        offerId: offerId, 
-        wantsToken: $(WETH), 
-        wants: cash(USDC, 1000), 
-        givesToken:$(USDC), 
-        gives:cash(WETH, 1), 
-        fee: 3000 } );
+        ArbParams memory params = ArbParams({
+            offerId: offerId,
+            takerWantsToken: $(WETH),
+            takerWants: cash(WETH, 1),
+            takerGivesToken: $(USDC),
+            takerGives: cash(USDC, 1000),
+            fee: 3000,
+            minGain:0
+        });
 
-    uint usdcBalanceBefore = USDC.balanceOf(taker);
-    uint wethBalanceBefore = WETH.balanceOf(taker);
-    uint amountOut = arbStrat.doArbitrage( params );
-    uint usdcBalanceAfter = USDC.balanceOf(taker);
-    uint wethBalanceAfter = WETH.balanceOf(taker);
-    assertTrue( usdcBalanceAfter > usdcBalanceBefore, "Should have increased usdcBalance " );
-    assertTrue( wethBalanceAfter == wethBalanceBefore, "Should have the same wethBalance" );
-    assertTrue( amountOut > params.wants, "Amount out should be larger than the initial offer on Mangrove" );
-  }
+        uint256 usdcBalanceBefore = USDC.balanceOf(address(arbStrat));
+        uint256 wethBalanceBefore = WETH.balanceOf(address(arbStrat));
+        uint256 amountOut = arbStrat.doArbitrage(params);
+        uint256 usdcBalanceAfter = USDC.balanceOf(address(arbStrat));
+        uint256 wethBalanceAfter = WETH.balanceOf(address(arbStrat));
+        assertTrue(usdcBalanceAfter > usdcBalanceBefore, "Should have increased usdcBalance ");
+        assertTrue(wethBalanceAfter == wethBalanceBefore, "Should have the same wethBalance");
+        assertTrue(amountOut > params.takerGives, "Amount out should be larger than the initial offer on Mangrove");
+    }
 
     function test_isNotProfitable() public {
-    deal( $(USDC), taker, cash(USDC, 20000));
-    deal( $(WETH), seller,cash(WETH, 10));
-    vm.prank(seller);
-    uint offerId = mgv.newOffer{value: 1 ether}({
-    outbound_tkn: $(WETH),
-    inbound_tkn: $(USDC),
-    wants: cash(USDC, 10000),
-    gives: cash(WETH, 1),
-    gasreq: 50_000 ,
-    gasprice: 0,
-    pivotId: 0
-    });
+        deal($(USDC), address(arbStrat), cash(USDC, 20000));
+        deal($(WETH), seller, cash(WETH, 10));
+        vm.prank(seller);
+        uint256 offerId = mgv.newOffer{value: 1 ether}({
+            outbound_tkn: $(WETH),
+            inbound_tkn: $(USDC),
+            wants: cash(USDC, 10000),
+            gives: cash(WETH, 1),
+            gasreq: 50_000,
+            gasprice: 0,
+            pivotId: 0
+        });
 
-    ArbParams memory params = ArbParams( { 
-        taker:taker, 
-        offerId: offerId, 
-        wantsToken: $(WETH), 
-        wants: cash(USDC, 1000), 
-        givesToken:$(USDC), 
-        gives:cash(WETH, 1), 
-        fee: 3000 } );
+        ArbParams memory params = ArbParams({
+            offerId: offerId,
+            takerWantsToken: $(WETH),
+            takerWants: cash(WETH, 1),
+            takerGivesToken: $(USDC),
+            takerGives: cash(USDC, 10000),
+            fee: 3000,
+            minGain:0
+        });
 
-    vm.expectRevert("Too little received");
-    arbStrat.doArbitrage( params );
-  }
+        vm.expectRevert("Too little received");
+        arbStrat.doArbitrage(params);
+    }
 
-  function test_offerFailedOnMangrove() public {
-    deal( $(USDC), taker, cash(USDC, 20000));
-    vm.prank(seller);
-    uint offerId = mgv.newOffer{value: 1 ether}({
-    outbound_tkn: $(WETH),
-    inbound_tkn: $(USDC),
-    wants: cash(USDC, 1000),
-    gives: cash(WETH, 1),
-    gasreq: 50_000 ,
-    gasprice: 0,
-    pivotId: 0
-    });
+    function test_offerFailedOnMangrove() public {
+        deal($(USDC), address(arbStrat), cash(USDC, 20000));
+        vm.prank(seller);
+        uint256 offerId = mgv.newOffer{value: 1 ether}({
+            outbound_tkn: $(WETH),
+            inbound_tkn: $(USDC),
+            wants: cash(USDC, 1000),
+            gives: cash(WETH, 1),
+            gasreq: 50_000,
+            gasprice: 0,
+            pivotId: 0
+        });
 
-    ArbParams memory params = ArbParams( { 
-        taker:taker, 
-        offerId: offerId, 
-        wantsToken: $(WETH), 
-        wants: cash(USDC, 1000), 
-        givesToken:$(USDC), 
-        gives:cash(WETH, 1), 
-        fee: 3000 } );
+        ArbParams memory params = ArbParams({
+            offerId: offerId,
+            takerWantsToken: $(WETH),
+            takerWants: cash(WETH, 1),
+            takerGivesToken: $(USDC),
+            takerGives: cash(USDC, 1000),
+            fee: 3000,
+            minGain:0
+        });
 
+        vm.expectRevert("MgvArbitrage/snipeFail");
+        arbStrat.doArbitrage(params);
+    }
 
-    vm.expectRevert("MgvArbitrage/snipeFail");
-    arbStrat.doArbitrage( params );
-  }
+    function test_isProfitable_exchangeDaiCurreny_Uniswap() public {
+        deal($(DAI), address(arbStrat), cash(DAI, 2000));
+        deal($(WETH), seller, cash(WETH, 10));
+        vm.prank(seller);
+        uint256 offerId = mgv.newOffer{value: 1 ether}({
+            outbound_tkn: $(WETH),
+            inbound_tkn: $(USDC),
+            wants: cash(USDC, 1000),
+            gives: cash(WETH, 1),
+            gasreq: 50_000,
+            gasprice: 0,
+            pivotId: 0
+        });
 
+        ArbParams memory params = ArbParams({
+            offerId: offerId,
+            takerWantsToken: $(WETH),
+            takerWants: cash(WETH, 1),
+            takerGivesToken: $(USDC),
+            takerGives: cash(USDC, 1000),
+            fee: 3000,
+            minGain:0
+        });
+
+        uint256 usdcBalanceBefore = USDC.balanceOf(address(arbStrat));
+        uint256 wethBalanceBefore = WETH.balanceOf(address(arbStrat));
+        uint256 daiBalanceBefore = DAI.balanceOf(address(arbStrat));
+        uint256 amountOut = arbStrat.doArbitrageExchangeOnUniswap(params, address(DAI), 100);
+        uint256 daiBalanceAfter = DAI.balanceOf(address(arbStrat));
+        uint256 usdcBalanceAfter = USDC.balanceOf(address(arbStrat));
+        uint256 wethBalanceAfter = WETH.balanceOf(address(arbStrat));
+        assertGt(usdcBalanceAfter, usdcBalanceBefore, "Should have increased usdcBalance ");
+        assertEq(wethBalanceAfter, wethBalanceBefore, "Should have the same wethBalance");
+        assertEq(daiBalanceAfter, daiBalanceBefore, "Should have the same daiBalance");
+        assertGt(amountOut, params.takerGives, "Amount out should be larger than the initial offer on Mangrove");
+    }
+
+    function test_isNotProfitable_exchangeDaiCurreny_Uniswap() public {
+        deal($(DAI), address(arbStrat), cash(DAI, 2000));
+        deal($(WETH), seller, cash(WETH, 10));
+        vm.prank(seller);
+        uint256 offerId = mgv.newOffer{value: 1 ether}({
+            outbound_tkn: $(WETH),
+            inbound_tkn: $(USDC),
+            wants: cash(USDC, 1000),
+            gives: cash(WETH, 1),
+            gasreq: 50_000,
+            gasprice: 0,
+            pivotId: 0
+        });
+
+        ArbParams memory params = ArbParams({
+            offerId: offerId,
+            takerWantsToken: $(WETH),
+            takerWants: cash(WETH, 1),
+            takerGivesToken: $(USDC),
+            takerGives: cash(USDC, 1000),
+            fee: 3000,
+            minGain:cash(USDC, 1000)
+        });
+
+        vm.expectRevert("MgvArbitrage/notMinGain");
+        arbStrat.doArbitrageExchangeOnUniswap(params, address(DAI), 100);
+    }
+
+        function test_isProfitable_exchangeDaiCurreny_Mgv() public {
+        deal($(DAI), address(arbStrat), cash(DAI, 2000));
+        deal($(WETH), seller, cash(WETH, 10));
+        vm.prank(seller);
+        uint256 offerId = mgv.newOffer{value: 1 ether}({
+            outbound_tkn: $(WETH),
+            inbound_tkn: $(USDC),
+            wants: cash(USDC, 1000),
+            gives: cash(WETH, 1),
+            gasreq: 50_000,
+            gasprice: 0,
+            pivotId: 0
+        });
+
+        ArbParams memory params = ArbParams({
+            offerId: offerId,
+            takerWantsToken: $(WETH),
+            takerWants: cash(WETH, 1),
+            takerGivesToken: $(USDC),
+            takerGives: cash(USDC, 1000),
+            fee: 3000,
+            minGain:0
+        });
+
+        uint256 usdcBalanceBefore = USDC.balanceOf(address(arbStrat));
+        uint256 wethBalanceBefore = WETH.balanceOf(address(arbStrat));
+        uint256 daiBalanceBefore = DAI.balanceOf(address(arbStrat));
+        uint256 amountOut = arbStrat.doArbitrageExchangeOnMgv(params, address(DAI));
+        uint256 daiBalanceAfter = DAI.balanceOf(address(arbStrat));
+        uint256 usdcBalanceAfter = USDC.balanceOf(address(arbStrat));
+        uint256 wethBalanceAfter = WETH.balanceOf(address(arbStrat));
+        assertGt(usdcBalanceAfter, usdcBalanceBefore, "Should have increased usdcBalance ");
+        assertEq(wethBalanceAfter, wethBalanceBefore, "Should have the same wethBalance");
+        assertEq(daiBalanceAfter, daiBalanceBefore, "Should have the same daiBalance");
+        assertGt(amountOut, params.takerGives, "Amount out should be larger than the initial offer on Mangrove");
+    }
+
+    function test_isNotProfitable_exchangeDaiCurreny_Mgv() public {
+        deal($(DAI), address(arbStrat), cash(DAI, 2000));
+        deal($(WETH), seller, cash(WETH, 10));
+        vm.prank(seller);
+        uint256 offerId = mgv.newOffer{value: 1 ether}({
+            outbound_tkn: $(WETH),
+            inbound_tkn: $(USDC),
+            wants: cash(USDC, 1000),
+            gives: cash(WETH, 1),
+            gasreq: 50_000,
+            gasprice: 0,
+            pivotId: 0
+        });
+
+        ArbParams memory params = ArbParams({
+            offerId: offerId,
+            takerWantsToken: $(WETH),
+            takerWants: cash(WETH, 1),
+            takerGivesToken: $(USDC),
+            takerGives: cash(USDC, 1000),
+            fee: 3000,
+            minGain:cash(USDC, 1000)
+        });
+
+        vm.expectRevert("MgvArbitrage/notMinGain");
+        arbStrat.doArbitrageExchangeOnMgv(params, address(DAI));
+    }
 }
